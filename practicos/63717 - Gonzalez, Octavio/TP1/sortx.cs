6aee852ae081faf3d6eddc83e5ab092756eec9d3
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-04-04 19:57:40 -0300

    primer commit: Empezando

--- practicos/63717 - Gonzalez, Octavio/TP1/sortx.cs
+++ practicos/63717 - Gonzalez, Octavio/TP1/sortx.cs
@@ -4,4 +4,14 @@
 //       [-d|--delimiter delimitador]
 //       [-nh|--no-header] [-h|--help]
 
+using System;
+
+//ignorar comentario... Record: es como una clase peero mas corta. 
+
+//record Empleado (int IDempleado, string nombre);  Estos valores no suelen cambiar, solo son de lectura. Si hay dos con los mismos datos, C# los considera iguales 
+
+
+
+
+
 Console.WriteLine($"sortx {string.Join(" ", args)}");
