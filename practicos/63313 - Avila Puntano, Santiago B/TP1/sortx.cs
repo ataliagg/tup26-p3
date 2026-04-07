120fb8b0996d206e8679dcb8ae1b8e0fc469167a
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-06 21:06:47 -0300

    Ingreso de la configuracion del programa.

--- practicos/63313 - Avila Puntano, Santiago B/TP1/sortx.cs
+++ practicos/63313 - Avila Puntano, Santiago B/TP1/sortx.cs
@@ -5,3 +5,14 @@
 //       [-nh|--no-header] [-h|--help]
 
 Console.WriteLine($"sortx {string.Join(" ", args)}");
+
+//ingreso config del programa - primer commit
+record SortField(string Name, bool Numeric, bool Descending);
+
+record AppConfig(
+    string?         InputFile,
+    string?         OutputFile,
+    string          Delimiter,
+    bool            NoHeader,
+    List<SortField> SortFields
+);
