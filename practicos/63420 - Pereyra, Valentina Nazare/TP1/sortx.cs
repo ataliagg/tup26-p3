3823dd94da89678bcdd092a830ae45e7032d1b21
IgnacioRosconi <123178572+IgnacioRosconi@users.noreply.github.com>
2026-04-08 19:23:23 -0300

    estructura inicial del programa con modelos de datos

--- practicos/63420 - Pereyra, Valentina Nazare/TP1/sortx.cs
+++ practicos/63420 - Pereyra, Valentina Nazare/TP1/sortx.cs
@@ -5,3 +5,45 @@
 //       [-nh|--no-header] [-h|--help]
 
 Console.WriteLine($"sortx {string.Join(" ", args)}");
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+record CampoOrden(string Nombre, bool Descendente);
+
+record Configuracion(string Entrada, string Salida, bool Descendente, List<CampoOrden> Campos);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Iniciando aplicación...");
+    }
+
+    static Configuracion ObtenerConfiguracion(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    static List<string> LeerArchivo(string ruta)
+    {
+        throw new NotImplementedException();
+    }
+
+    static List<string> OrdenarLineas(List<string> lineas, Configuracion config)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void GuardarArchivo(string ruta, List<string> lineas)
+    {
+        throw new NotImplementedException();
+    }
+
+    static void MostrarAyuda()
+    {
+        throw new NotImplementedException();
+    }
+}
