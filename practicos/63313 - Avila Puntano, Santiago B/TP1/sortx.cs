a8ce41b814a791a235baba15d4dfb705a3c747c6
bnjavila07 <bnjavilapuntano@gmail.com>
2026-04-08 10:16:43 -0300

    parseo de datos

--- practicos/63313 - Avila Puntano, Santiago B/TP1/sortx.cs
+++ practicos/63313 - Avila Puntano, Santiago B/TP1/sortx.cs
@@ -4,15 +4,43 @@
 //       [-d|--delimiter delimitador]
 //       [-nh|--no-header] [-h|--help]
 
+using System.Net.Http.Headers;
+using Microsoft.VisualBasic.FileIO;
+
 Console.WriteLine($"sortx {string.Join(" ", args)}");
 
-//ingreso config del programa - primer commit
+// este appconfig guarda los datos temporalmente, a diferencia del record que los toma al final
+AppConfig parseargs(string[] args)
+{
+    string? inputFile = null; 
+    string? outputFile = null; 
+    string deLimiter = ","; 
+    bool noHeader = false; 
+    bool showHelp = false;
+    List<SortField> sortFields = new List<SortField>(); 
+    int positional = 0; 
+
+    SortField ParseSortField(string spec)
+    {
+        var parts = spec.Split(':');
+        string name = parts[0];
+        bool numeric = parts.Length > 1 && parts[1].Equals("num", StringComparison.OrdinalIgnoreCase);
+        bool descending = parts.Length > 2 && parts[2].Equals("desc", StringComparison.OrdinalIgnoreCase);
+        return new SortField(name, numeric, descending);}
+    
+
+    for (int i = 0; i < args.Length; i++)
+
+ //commit 1
 record SortField(string Name, bool Numeric, bool Descending);
 
 record AppConfig(
-    string?         InputFile,
-    string?         OutputFile,
-    string          Delimiter,
-    bool            NoHeader,
+    string? InputFile,
+    string? OutputFile,
+    string Delimiter,
+    bool NoHeader,
+    bool ShowHelp,
     List<SortField> SortFields
 );
+
+
