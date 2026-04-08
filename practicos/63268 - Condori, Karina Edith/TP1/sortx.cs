8f398f79f916434579f78b61fc45a41a5f3c953f
Karina, Edith Condori <kariiedith9315@gmail.com>
2026-04-08 16:35:09 -0300

    Primer Commit
    Representa un criterio de ordenamiento,se ordena por nombre, numero y de forma descendente
    El record AppConfig guarda la configuracion del programa, puede ser entrada o salida, los caracter estan delimitados y no contiene un encabezado, se ordena por una lista de ordenamiento

--- practicos/63268 - Condori, Karina Edith/TP1/sortx.cs
+++ practicos/63268 - Condori, Karina Edith/TP1/sortx.cs
@@ -1,7 +1,12 @@
-
-// sortx [input [output]] [-b|--by campo[:tipo[:orden]]]...
-//       [-i|--input input] [-o|--output output]
-//       [-d|--delimiter delimitador]
-//       [-nh|--no-header] [-h|--help]
-
+using System;
+using System.Linq;
 Console.WriteLine($"sortx {string.Join(" ", args)}");
+record SortField(string Name, bool Numeric, bool Descending);
+
+record AppConfig(
+    string? InputFile,
+    string? OutputFile,
+    string Delimiter,
+    bool NoHeader,
+    List<SortField> SortFields
+);
