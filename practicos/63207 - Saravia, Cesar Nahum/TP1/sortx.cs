c7527d31fbeded0a469bdea0e2098b6e48334cf6
Nahum-Saravia <nahum-s@hotmail.com>
2026-04-07 23:57:32 -0300

    Implementar ParseArgs y PrintHelp

--- practicos/63207 - Saravia, Cesar Nahum/TP1/sortx.cs
+++ practicos/63207 - Saravia, Cesar Nahum/TP1/sortx.cs
@@ -15,6 +15,79 @@ catch (Exception ex)
     Environment.Exit(1);
 }
 
+AppConfig ParseArgs(string[] args)
+{
+    string? input = null;
+    string? output = null;
+    string delimiter = ",";
+    bool noHeader = false;
+    var fields = new List<SortField>();
+    var positional = new List<string>();
+
+    for (int i = 0; i < args.Length;)
+    {
+        var arg = args[i];
+
+        switch (arg)
+        {
+            case "-h":
+            case "--help":
+                PrintHelp();
+                Environment.Exit(0);
+                break;
+
+            case "-i":
+            case "--input":
+                input = Next(args, ref i, arg);
+                break;
+
+            case "-o":
+            case "--output":
+                output = Next(args, ref i, arg);
+                break;
+
+            case "-d":
+            case "--delimiter":
+                delimiter = Next(args, ref i, arg);
+                if (delimiter == "\\t") delimiter = "\t";
+                break;
+
+            case "-nh":
+            case "--no-header":
+                noHeader = true;
+                i++;
+                break;
+
+            case "-b":
+            case "--by":
+                fields.Add(ParseSortField(Next(args, ref i, arg)));
+                break;
+
+            default:
+                if (arg.StartsWith("-"))
+                    throw new Exception($"Opción desconocida: {arg}");
+
+                positional.Add(arg);
+                i++;
+                break;
+        }
+    }
+
+    if (positional.Count >= 1 && input == null) input = positional[0];
+    if (positional.Count >= 2 && output == null) output = positional[1];
+
+    if (fields.Count == 0)
+        throw new Exception("Debe indicar al menos un campo con -b");
+
+    return new AppConfig(input, output, delimiter, noHeader, fields);
+}
+
+void PrintHelp()
+{
+    Console.WriteLine("Uso: sortx [input [output]] -b campo[:tipo[:orden]]");
+}
+
+
 record SortField(string Name, bool Numeric, bool Descending);
 
 record AppConfig(
