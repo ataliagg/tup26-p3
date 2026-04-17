e3fc7745e25bd81f75616b1a0c05c63d2bc0ea43
Atalia Gil Garau <ataliaggcol@gmail.com>
2026-04-17 16:13:02 -0300

    Update Compilador.cs

--- practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Compilador.cs
+++ practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Compilador.cs
@@ -1,5 +1,46 @@
 class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+    public static Nodo Parse(string expresion) { 
+        // prepara token / cursor para recorrer la expresión
+        return ParseExpresion();
     }
+
+    Nodo ParseExpresion() {
+        var nodo = ParseTermino();
+        while (token == '+' || token == '-') {
+            string operador = token;
+            AvanzarToken();
+            var terminoDerecho = ParseTermino();
+            nodo = new NodoOperacion( nodo, operador, terminoDerecho);
+        }
+        return nodo;    
+    }
+    Nodo ParseTermino() {
+        var nodo = ParseFactor();
+        while (token == '*' || token == '/') {
+            string operador = token;
+            AvanzarToken();
+            var terminoDerecho = ParseFactor();
+            nodo = new NodoOperacion( operador, nodo, terminoDerecho);
+        }
+        return nodo;    
+    }
+       
+
+
+
+
+
+
 }
+
+
+/*
+Expresion := Termino { ('+' | '-') Termino }
+Termino   := Factor  { ('*' | '/') Factor }
+Factor    := '+' Factor
+          | '-' Factor
+          | '(' Expresion ')'
+          | numero
+          | x
+
+*/
