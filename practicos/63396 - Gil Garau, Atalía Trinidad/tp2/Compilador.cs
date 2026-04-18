df2e444dbd16825e0324b627eff913a01d7c1870
Atalia Gil Garau <ataliaggcol@gmail.com>
2026-04-18 20:09:24 -0300

    empiezo con los nodos

--- practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Compilador.cs
+++ practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Compilador.cs
@@ -1,10 +1,76 @@
 class Compilador {
-    char[] tokens (string expresion) {
-        // Implementar tokenización aquí
+
+    enum TipoToken {
+        Numero, Variable,
+        Suma, Resta,
+        Multiplicacion, Division,
+        ParentesisAbierto, ParentesisCerrado,
+        Final
     }
-    tekenizador Tokenizar(string expresion) {
-        // Implementar tokenización aquí
+    record Token(TipoToken Tipo, string Valor = "");
+
+    List<Token> Tokenizar(string expresion) {
+        int posicion = 0;
+        char continuar() => expresion[posicion++];
+
+        List<Token> token = new();
+
+        while (posicion < expresion.Length) {
+            char c = expresion[posicion];
+
+            if (char.IsWhiteSpace(c)) {
+             continuar();
+                continue;
+            }
+
+           if (char.IsDigit(c)) {
+                string numero = "";
+                while (posicion < expresion.Length && char.IsDigit(expresion[posicion])) {
+                    numero += continuar();
+                }
+                token.Add(new Token(TipoToken.Numero, numero));
+                continue;
+            }
+
+            if (c == 'x' || c == 'X') {
+                token.Add(new Token(TipoToken.Variable, c.ToString()));
+                continuar();
+                continue;
+            }
+
+            switch (c) {
+                case '+':
+                    token.Add(new Token(TipoToken.Suma));
+                    break;
+                case '-':
+                    token.Add(new Token(TipoToken.Resta));
+                    break;
+                case '*':
+                    token.Add(new Token(TipoToken.Multiplicacion));
+                    break;
+                case '/':
+                    token.Add(new Token(TipoToken.Division));
+                    break;
+                case '(':
+                    token.Add(new Token(TipoToken.ParentesisAbierto));
+                    break;
+                case ')':
+                    token.Add(new Token(TipoToken.ParentesisCerrado));
+                    break;
+                default:
+                    throw new Exception($"Ingreso carácter inesperado: {c}");
+            }
+
+            continuar();
+        }
+
+        token.Add(new Token(TipoToken.Final));
+        return token;
     }
+
+    // public static Nodo Parse(string expresion) {
+    //  throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+    //}
     Nodo ParseTermino() {
         var nodo = ParseFactor();
         while (token == '*' || token == '/') {
@@ -55,15 +121,7 @@ class Compilador {
         }
         throw new Exception("Token inesperado: " + token);
     }
-
-
-
-
-
-
 }
-
-
 /*
 Expresion := Termino { ('+' | '-') Termino }
 Termino   := Factor  { ('*' | '/') Factor }
--- practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Nodos.cs
+++ practicos/63396 - Gil Garau, Atalía Trinidad/tp2/Nodos.cs
@@ -1,3 +1,37 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+sealed class NumeroNodo : Nodo {
+    int valor;
+    public NumeroNodo(int valor) {
+        this.valor = valor;
+    }
+    public override int Evaluar(int x = 0) => valor;
+}
+sealed class VariableNodo : Nodo {
+    
+}
+sealed class NegativoNodo : Nodo
+{
+    
+}
+abstract class BinarioNodo : Nodo
+{
+    
+}
+sealed class SumaNodo : Nodo
+{
+    
+}
+sealed class RestaNodo : Nodo
+{
+    
+}
+sealed class MultiplicacionNodo : Nodo
+{   
+    
+}
+sealed class DivisionNodo : Nodo
+{   
+    
+}
